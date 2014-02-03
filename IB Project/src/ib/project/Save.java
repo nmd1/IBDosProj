2da2e7d03997c72cc1e8b39cf54ca3e6d80a451d
nmd <Rafty132@gmail.com>
2014-02-03 13:27:00 -0500

    Finished File folder and Save
    Finished the file folder system to save quizzes. Also Finished working
    on the Save file

--- IB Project/src/ib/project/Save.java
+++ IB Project/src/ib/project/Save.java
@@ -16,10 +16,11 @@ import java.util.logging.Logger;
 
 public class Save {
     //global variables
-    Button SavetoText, SaveandTake, TakeQuiz;
+    Button SavetoText, SaveandTake, TakeQuiz, leave;
     Container saverPane;
     JFileChooser fc = new JFileChooser();
     SpringLayout layout = new SpringLayout();
+    Label Sucess = new Label();
     public void Saving() {
         prop.setVisible(false);
         main.setVisible(false);
@@ -39,17 +40,25 @@ public class Save {
         SavetoText = new Button("Save to a text file for later");saverPane.add(SavetoText);
         SaveandTake = new Button("Save to a text file and take quiz now"); saverPane.add(SaveandTake);
         TakeQuiz = new Button("Take the quiz without saving quiz info"); saverPane.add(TakeQuiz);
+        leave = new Button("Exit");
+        leave.setPreferredSize(new Dimension(100,40));
         SavetoText.setPreferredSize(a);
         SaveandTake.setPreferredSize(a);
         TakeQuiz.setPreferredSize(a);
         SavetoText.addActionListener(new StT());
+        SaveandTake.addActionListener(new SaT());
+        TakeQuiz.addActionListener(new TQ());
+        leave.addActionListener(new Exit());
     }
     public void Layout(Component c, int x, int y) {
         layout.putConstraint(SpringLayout.WEST, c,x, SpringLayout.WEST, saverPane);
         layout.putConstraint(SpringLayout.NORTH, c,y,SpringLayout.NORTH, saverPane);
     }
-    public void file() {
+    public void file(String a) {
         fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        fc.setApproveButtonText(a);
+        fc.setApproveButtonMnemonic('S');
+        fc.setApproveButtonToolTipText("Save the \" Data.txt \" file in this current folder");
           int returnVal = fc.showOpenDialog(saverPane);
           if (returnVal == JFileChooser.APPROVE_OPTION) {
             File file = new File(fc.getSelectedFile().getAbsolutePath() + "/DATA.txt"); //fileChooser.getSelectedFile();
@@ -60,25 +69,27 @@ public class Save {
                 BufferedWriter output = new BufferedWriter(new FileWriter(file));
                 System.out.println(file.toPath());
                 output.write("QUIZ DATA\n");
-                for(String a: properties){
-                    output.write(a+"\n");
-                }
-                output.write("\n");
-                for(String a: QuestA){
-                    output.write(a);
-                }
-                output.write("\n");
-                for(String a: RAnswer){
-                    output.write(a);
-                }
-                output.write("\n");
-                for(ArrayList b : WAnswer){
-                    //I WAS WORKING ON THIS LINE b.
-                    for(Object a : b){
-                        
-                    }
-                }
+                output.newLine();
+                output.write("" + properties);
+                output.newLine();
+                output.write("" + QuestA);
+                output.newLine();
+                output.write("" + RAnswer);
+                output.newLine();
+                output.write("" + WAnswer);
                 output.close();
+                
+                saverPane.remove(SaveandTake);
+                saverPane.remove(SavetoText);
+                saverPane.remove(TakeQuiz);
+                saverPane.add(Sucess);
+                Layout(Sucess, 10, 10);
+                Sucess.setText("Your quiz has been saved sucessfully");
+                Font f = new Font("Verdana", Font.BOLD, 24);
+                Sucess.setFont(f);
+                saverPane.add(leave);
+                Layout(leave, 130, 120);
+                
             } catch (IOException e) {
                 System.out.println("problem accessing file"+file.getAbsolutePath());
                 /*int choice = */JOptionPane.showConfirmDialog(null, 
@@ -93,7 +104,35 @@ public class Save {
     private class StT implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent ae) {
-            file();
+            file("Save here");
+        }
+    }
+    private class SaT implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            file("Save and Take Quiz");
+        }
+    }
+    private class TQ implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            //go directly to take screen
+        }
+    }
+    private class Exit implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            int choice = JOptionPane.showConfirmDialog(null, 
+                        "Would you like to exit the QuizCreater? No for mainscreen", 
+                        "Exit", JOptionPane.YES_NO_OPTION);// yes = 0 no = 1
+            if(choice == 0) System.exit(choice); //WOOO EXIT POINT
+            if(choice == 1) {
+                if(debug = true) System.out.println("Main Menu");    
+                save.setVisible(false);
+                main.setVisible(true);
+                prop.setVisible(false);
+            
+            }
         }
     }
 }
